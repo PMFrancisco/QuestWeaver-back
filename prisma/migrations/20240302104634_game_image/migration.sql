ef430e07a38f2308b7ed42b41dc868fb4ae1f78f
PMFrancisco <francisco.perez.munoz@gmail.com>
2024-03-02 11:59:21 +0100

    feat(db): added gameImage to the Game model

---
+++ prisma/migrations/20240302104634_game_image/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Game" ADD COLUMN     "gameImage" TEXT DEFAULT 'https://res.cloudinary.com/du8nkdwcp/image/upload/v1709376290/QuestWeaver/tabletop_default_image_small_lmpbok.jpg';
